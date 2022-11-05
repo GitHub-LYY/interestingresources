08c9c49202346b08e02900fc20da93b86ba3b06d
yangyangliu <2676028176@qq.com>
2022-11-05 21:50:27 +0800

    update leetcode 8

--- datastructure/string/8.cpp
+++ datastructure/string/8.cpp
@@ -4,6 +4,7 @@
 ** Description: 8. 字符串转换整数 (atoi)
 ** link: https://leetcode.cn/problems/string-to-integer-atoi/
 ** reference: 题解区，搜索，【数据结构和算法】字符串转换整数 (atoi)
+** 20221105，自己再次做，有了自己的理解，又写了一个版本
 */
 
 #include <iostream>
@@ -12,7 +13,7 @@ using namespace std;
 
 class Solution {
 public:
-    int myAtoi(string s) {
+    int myAtoiOld(string s) {
         // 如果字符串为空，则返回0
         if (s.size() == 0) {
             return 0;
@@ -66,6 +67,52 @@ public:
         }
 
         return res * sign;
+    }
+
+    int myAtoi(string s) {
+        // 定义变量保存索引用来遍历字符串
+        int i = 0;
+
+        // 定义变量保存正负数标记，使用1标记正数，-1标记负数，方便后边直接与数相乘求着个数
+        int sign = 1;
+
+        // 遍历前边的空格
+        while (i < s.size() && s[i] == ' ') {
+            i++;
+        }
+
+        // 字符串的题不要忘了判断索引是否越界
+        if (i == s.size()) {
+            return 0;
+        }
+
+        // 判断正负号，条件中使用 或 运算，内部的写法很巧妙
+        if (i < s.size() && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '+' ? 1 : -1); // 巧妙
+            i++;
+        }
+
+        // 定义变量保存结果
+        long num = 0;
+
+        // 遍历数字区间
+        while (i < s.size() && isdigit(s[i])) {
+            num *= 10;
+            num += s[i] - '0';
+
+            // 这儿不要忘了每个循环要判断当前是否越界，越界了要及时退出
+            if (num * sign > INT_MAX) {
+                return INT_MAX;
+            }
+
+            if (num * sign < INT_MIN) {
+                return INT_MIN;
+            }
+
+            i++;
+        }
+
+        return num * sign; // 这可以解决测试用例"-+50"
     }
 };
 
